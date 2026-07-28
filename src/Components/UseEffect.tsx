8f08063212a237d1533ddb1a230577c0b72f315a
sitikhumaera <sitikhumaera.1601@gmail.com>
2026-07-28 14:48:21 +0700

    menambahkan tanggal dan waktu

--- src/Components/UseEffect.tsx
+++ src/Components/UseEffect.tsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function UseEffect(){
+     const [dateTime, setDateTime] = useState("");
     useEffect(() =>{
         console.log("Day 1 nihh HAHAHHAHUHUHUHU")
+        const sekarang = new Date();
+        setDateTime(sekarang.toLocaleString());
     }, []);
     return(
         <div>
             <h1> Day 1 useEffect</h1>
+            <p>Tanggal & Waktu</p>
+            <p>{dateTime}</p>
         </div>
     );
 }
