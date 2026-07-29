5dc9367e0cf9b8b99c400ab0baed30263005a74b
sitikhumaera <sitikhumaera.1601@gmail.com>
2026-07-29 09:17:06 +0700

    nambahin loading dan dashboard

--- src/Components/Conditional.tsx
+++ src/Components/Conditional.tsx
@@ -2,13 +2,34 @@ import { useState } from "react";
 
 function Conditional() {
   const [isLogin, setIsLogin] = useState(false);
+  const [loading, setLoading] = useState(false);
+
 
   const handleLogin = () => {
-    setIsLogin(!isLogin);
+    if (!isLogin){
+        setLoading(true);
+
+        setTimeout(() => {
+        setLoading(false);
+        setIsLogin(true);
+        }, 2000);
+    }else{
+        setIsLogin(false);
+    }
   };
 
   return (
     <div>
+        {loading ? (
+        <h2>Loading...</h2>
+      ) : isLogin ? (
+        <h2>Dashboard</h2>
+      ) : (
+        <h2>Silakan Login</h2>
+      )}
+      {isLogin && !loading && (
+      <p>Selamat datang di Dashboard 🎉</p>)}
+
       <button onClick={handleLogin}>
         {isLogin ? "Logout" : "Login"}
       </button>
